79ff27c2e99a9d5eb5585fd26e92459a8a8c7791
laurent <htd.laurent@gmail.com>
2018-01-19 17:27:00 +0100

    template edit

--- base/SpinalDrive_App_edit.js
+++ base/SpinalDrive_App_edit.js
@@ -41,37 +41,58 @@ SpinalDrive_App_Edit.DialogEditCtrl = function ($scope, $mdDialog, model_server_
   };
 };
 
-SpinalDrive_App_Edit.EditTemplate = '<md-dialog aria-label="Sharing setting">' +
-  '  <md-dialog-content style="padding-left: 10px;padding-right: 10px;">' +
-  '        <h3>Edit Setting</h3>' +
-  '        <h5 style="margin-top: 30px;margin-bottom: 35px;">Type: {{editModelContruct}}</h5>' +
-  '<form name="editForm" ng-submit="submitDialog()">' +
-  '    <md-input-container ng-if="isStr(editModel)" style="margin: 0;" class="md-block">' +
-  '      <label>Data</label>' +
-  '      <input ng-model="modelData" name="modelData" md-autofocus>' +
-  '    </md-input-container>' +
-  '    <md-input-container ng-if="isVal(editModel)" style="margin: 0;" class="md-block">' +
-  '      <label>Data</label>' +
-  '      <input type="number" name="modelData"  ng-model="modelData" md-autofocus>' +
-  '    </md-input-container>' +
-  '    <md-input-container ng-if="isBool(editModel)"style="margin: 0;" class="md-block">' +
-  '      <label>Data</label>' +
-  '      <md-select name="modelData"ng-model="modelData" md-autofocus>' +
-  '        <md-option value=true>true</md-option>' +
-  '        <md-option value=false>false</md-option>' +
-  '      </md-select>' +
-  '    </md-input-container>' +
-  ' <p class="p-error fadein" ng-repeat="error_msg in error_msgs">{{error_msg}}</p>' +
-  '  </md-dialog-content>' +
-  '  <md-dialog-actions>' +
-  '    <md-buttongfngf ng-click="cancelDialog()" class="md-primary">' +
-  '      Cancel' +
-  '    </md-button>' +
-  '    <md-button ng-click="submitDialog()" class="md-primary">' +
-  '      Submit' +
-  '    </md-button>' +
-  '  </md-dialog-actions>' +
-  '</md-dialog>';
+angular.module('app.spinal-pannel')
+  .run(["$templateCache", "$http",
+    function ($templateCache, $http) {
+      let load_template = (uri, name) => {
+        $http.get(uri).then((response) => {
+          $templateCache.put(name, response.data);
+        }, (errorResponse) => {
+          console.log('Cannot load the file ' + uri);
+        });
+      };
+      let toload = [{
+        uri: '../templates/spinal-env-drive-plugin-base/SpinalDrive_App_Edit_EditTemplate.html',
+        name: 'SpinalDrive_App_Edit_EditTemplate.html'
+      }];
+      for (var i = 0; i < toload.length; i++) {
+        load_template(toload[i].uri, toload[i].name);
+      }
+    }
+  ])
+
+
+// SpinalDrive_App_Edit.EditTemplate = '<md-dialog aria-label="Sharing setting">' +
+//   '  <md-dialog-content style="padding-left: 10px;padding-right: 10px;">' +
+//   '        <h3>Edit Setting</h3>' +
+//   '        <h5 style="margin-top: 30px;margin-bottom: 35px;">Type: {{editModelContruct}}</h5>' +
+//   '<form name="editForm" ng-submit="submitDialog()">' +
+//   '    <md-input-container ng-if="isStr(editModel)" style="margin: 0;" class="md-block">' +
+//   '      <label>Data</label>' +
+//   '      <input ng-model="modelData" name="modelData" md-autofocus>' +
+//   '    </md-input-container>' +
+//   '    <md-input-container ng-if="isVal(editModel)" style="margin: 0;" class="md-block">' +
+//   '      <label>Data</label>' +
+//   '      <input type="number" name="modelData"  ng-model="modelData" md-autofocus>' +
+//   '    </md-input-container>' +
+//   '    <md-input-container ng-if="isBool(editModel)"style="margin: 0;" class="md-block">' +
+//   '      <label>Data</label>' +
+//   '      <md-select name="modelData"ng-model="modelData" md-autofocus>' +
+//   '        <md-option value=true>true</md-option>' +
+//   '        <md-option value=false>false</md-option>' +
+//   '      </md-select>' +
+//   '    </md-input-container>' +
+//   ' <p class="p-error fadein" ng-repeat="error_msg in error_msgs">{{error_msg}}</p>' +
+//   '  </md-dialog-content>' +
+//   '  <md-dialog-actions>' +
+//   '    <md-buttongfngf ng-click="cancelDialog()" class="md-primary">' +
+//   '      Cancel' +
+//   '    </md-button>' +
+//   '    <md-button ng-click="submitDialog()" class="md-primary">' +
+//   '      Submit' +
+//   '    </md-button>' +
+//   '  </md-dialog-actions>' +
+//   '</md-dialog>';
 
 
 /**
@@ -102,7 +123,7 @@ class SpinalDrive_App_Inspector_edit extends SpinalDrive_App_Edit {
     mdDialog.show({
       ariaLabel: 'Edit',
       controller: ["$scope", "$mdDialog", "model_server_id", "spinalModelDictionary", "mdDialog", "ngSpinalCore", SpinalDrive_App_Edit.DialogEditCtrl],
-      template: SpinalDrive_App_Edit.EditTemplate,
+      template: "SpinalDrive_App_Edit_EditTemplate.html",
       parent: angular.element(document.body),
       clickOutsideToClose: true,
       locals: {
