f92adba62c26906f112aa8168773e6ee2a07e23b
laurent <htd.laurent@gmail.com>
2018-01-23 18:01:48 +0100

    rename file

--- spinaldrive_env_newFolder.js
+++ spinaldrive_env_newFolder.js
@@ -1,6 +1,6 @@
 (function () {
-  var SpinalDrive_App_FolderExplorer_newFolder = require('./base/SpinalDrive_app_newFolder.js').FolderExplorerNewFolder;
+  var SpinalDrive_App_FolderExplorer_newFolder = require('./base/SpinalDrive_App_newFolder.js').FolderExplorerNewFolder;
   spinalDrive_Env.add_applications('FolderExplorer', new SpinalDrive_App_FolderExplorer_newFolder());
-  var SpinalDrive_App_FileExplorer_currdir_newFolder = require('./base/SpinalDrive_app_newFolder.js').FileExplorerCurrDirNewFolder;
+  var SpinalDrive_App_FileExplorer_currdir_newFolder = require('./base/SpinalDrive_App_newFolder.js').FileExplorerCurrDirNewFolder;
   spinalDrive_Env.add_applications('FileExplorerCurrDir', new SpinalDrive_App_FileExplorer_currdir_newFolder());
 })();
--- spinaldrive_env_rename.js
+++ spinaldrive_env_rename.js
@@ -1,4 +1,4 @@
 (function () {
-  var SpinalDrive_App_FileExplorer_rename = require('./base/SpinalDrive_app_renameFile.js').FileExplorerRename;
+  var SpinalDrive_App_FileExplorer_rename = require('./base/SpinalDrive_App_renameFile.js').FileExplorerRename;
   spinalDrive_Env.add_applications('FileExplorer', new SpinalDrive_App_FileExplorer_rename());
 })();
